6c6830fe5b989703a14a3291359085814d2f3cd8
tarek <tarek@192.168.1.104>
2020-03-11 13:09:46 +0100

    The principal page (structure html and css).

--- index.php
+++ index.php
@@ -1,15 +1,59 @@
 <!DOCTYPE html>
-<!--
-Project of Tarek, Photogram.
--->
-<html>
+<!--Project of Tarek, Photogram.-->
+<html lang="es">
     <head>
-        <meta charset="UTF-8">
-        <title>Photogram.</title>
+        <!--Latin characters-->
+        <meta charset="UTF-8">       
+        <!--Title of the page.-->
+        <title>Photogram.</title>  
+        <!--( Etiqueta == Label ) for phone or tablet-->
+        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0">
+        <!--Links of CSS or JAVASCRIPT--> 
+        <link rel="stylesheet" href="css/style.css" type="text/css">
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <!--wrapper == envoltorio-->
+        <div id="wrapper">
+            <!--Left-->
+            <div class="w-left"><img src="images/celulares.png"></div>
+            <!--Right-->
+            <div class="w-right">
+                <?php
+                    // put your code here
+                ?> 
+                <div class="main-content">
+                    <!--Logo-->
+                    <div class="header">
+                        <img src="images/logo.png">
+                    </div>
+                    <!--Part number one-->
+                    <div class="l-part">
+                        <!--Login form-->
+                        <form action="" method="post">
+                            <!--User-->
+                            <input type="text" placeholder="Usuario" class="input" name="usuario" />
+                            <!--Password-->
+                            <input type="password" placeholder="Contraseña" class="input" name="password" />
+                            <!--(olvidar == forget) password or user name-->
+                            <a href="#">Olvidastes?</a>
+                            <!--Button for login-->
+                            <input type="submit" value="Entrar" class="btn" name="entrar" />
+                        </form>
+                    </div>
+                </div>    
+                <!--Foot part-->
+                <div class="sub-content">
+                    <!--Part number two-->
+                    <div class="s-part">
+                        <!--Sign in new user-->
+                        ¿No tienes una cuenta? <a href="registro.php">Regístrate</a>
+                    </div>
+                </div>
+                <center>
+                    <img src="images/appstores.png">
+                </center>
+            </div>
+        </div>
+
     </body>
 </html>
